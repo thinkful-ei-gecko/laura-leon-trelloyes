43dd14166a12847958a721a15266a115acf33374
laura <laura.j.elias@gmail.com>
2019-09-03 20:46:28 -0500

    corrections completed

--- src/composition/List.js
+++ src/composition/List.js
@@ -9,7 +9,19 @@ function List(props) {
           <h2>{props.header}</h2>
         </header>
         <div className="List-cards">
-          {props.cards}
+          {props.cards.map((card) =>
+            <Card 
+              key={card.id}
+              title={card.title}
+              content={card.content}
+              />
+          )}
+          <button 
+            type='button'
+            className='List-add-button'
+            >
+              + Add Random Card
+            </button>
         </div>
       </section>
   );
